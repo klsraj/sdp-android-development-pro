e12921b65b3c204e9bc2cd1f525bbaaec201dd7b
jwong91 <jwong91@gatech.edu>
2020-10-12 20:20:08 -0400

    Added persistence for weights classes

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/WeightsActivity.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/WeightsActivity.java
@@ -3,9 +3,8 @@ package edu.gatech.seclass.jobcompare6300;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.Editable;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,16 +14,40 @@ public class WeightsActivity extends AppCompatActivity {
 
     private Button button_cancel_weight;
     private Button button_save_weight;
-    private EditText commute_time;
-    private EditText yearly_salary;
-    private EditText yearly_bonus;
-    private EditText retirement;
-    private EditText leave;
+    private EditText text_commute_time;
+    private EditText text_yearly_salary;
+    private EditText text_yearly_bonus;
+    private EditText text_retirement;
+    private EditText text_leave;
+
+    public static final String SHARED_PREFS_COMMUTE = "spcommute";
+    public static final String SHARED_PREFS_SALARY = "spsalary";
+    public static final String SHARED_PREFS_BONUS = "spbonus";
+    public static final String SHARED_PREFS_RETIREMENT = "spretirement";
+    public static final String SHARED_PREFS_LEAVE = "spleave";
+
+    public static final String shared_commute_time = "text";
+    public static final String shared_yearly_salary = "text";
+    public static final String shared_yearly_bonus = "text";
+    public static final String shared_retirement = "text";
+    public static final String shared_leave = "text";
+
+    private String saved_commute_time;
+    private String saved_yearly_salary;
+    private String saved_yearly_bonus;
+    private String saved_retirement;
+    private String saved_leave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weights);
+
+        text_commute_time = (EditText) findViewById(R.id.text_commute_weight);
+        text_yearly_salary = (EditText) findViewById(R.id.text_salary_weight);
+        text_yearly_bonus = (EditText) findViewById(R.id.text_bonus_weight);
+        text_retirement = (EditText) findViewById(R.id.text_retirement_weight);
+        text_leave = (EditText) findViewById(R.id.text_leave_weight);
 
         button_cancel_weight = findViewById(R.id.button_cancel_weight);
         button_cancel_weight.setOnClickListener(new View.OnClickListener() {
@@ -33,7 +56,7 @@ public class WeightsActivity extends AppCompatActivity {
                 startActivity(intent);
                 Toast.makeText(getApplicationContext(),
                         "Entered weights cancelled",
-                        Toast.LENGTH_LONG)
+                        Toast.LENGTH_SHORT)
                         .show();
             }
         });
@@ -43,39 +66,105 @@ public class WeightsActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // Code that involves saving the user-entered weights here
                 //
-                saveWeight(view);
+                //saveWeight(view);
+                saveData();
                 Intent intent = new Intent(WeightsActivity.this, MainActivity.class);
                 startActivity(intent);
                 Toast.makeText(getApplicationContext(),
                         "Entered weights saved",
-                        Toast.LENGTH_LONG)
+                        Toast.LENGTH_SHORT)
                         .show();
             }
         });
+
+        loadData();
+        updateViews();
     }
 
-    public void saveWeight(View view){
-        commute_time = (EditText) findViewById(R.id.text_commute_weight);
-        int commute_timeText = Integer.parseInt(commute_time.getText().toString());
+//    public void saveWeight(View view){
+//        text_commute_time = (EditText) findViewById(R.id.text_commute_weight);
+//        int commute_timeText = Integer.parseInt(text_commute_time.getText().toString());
+//
+//        text_yearly_salary = (EditText) findViewById(R.id.text_salary_weight);
+//        int salary = Integer.parseInt(text_yearly_salary.getText().toString());
+//
+//        text_yearly_bonus = (EditText) findViewById(R.id.text_bonus_weight);
+//        int bonus = Integer.parseInt(text_yearly_bonus.getText().toString());
+//
+//        text_retirement = (EditText) findViewById(R.id.text_retirement_weight);
+//        int retirementValue = Integer.parseInt(text_retirement.getText().toString());
+//
+//        text_leave = (EditText) findViewById(R.id.text_leave_weight);
+//        int leaveValue = Integer.parseInt(text_leave.getText().toString());
+//
+//        Weights.setCommute_weight(commute_timeText);
+//        Weights.setSalary_weight(salary);
+//        Weights.setBonus_weight(bonus);
+//        Weights.setRetirement_weight(retirementValue);
+//        Weights.setLeave_weight(leaveValue);
+//
+//        Log.v("Text",Weights.printWeights());
+//    }
 
-        yearly_salary = (EditText) findViewById(R.id.text_salary_weight);
-        int salary = Integer.parseInt(yearly_salary.getText().toString());
+    public void saveData() {
+        SharedPreferences sp;
+        SharedPreferences.Editor ed;
 
-        yearly_bonus = (EditText) findViewById(R.id.text_bonus_weight);
-        int bonus = Integer.parseInt(yearly_bonus.getText().toString());
+        sp = getSharedPreferences(SHARED_PREFS_COMMUTE, MODE_PRIVATE);
+        ed = sp.edit();
+        ed.putString(shared_commute_time, text_commute_time.getText().toString());
+        ed.commit();
 
-        retirement = (EditText) findViewById(R.id.text_retirement_weight);
-        int retirementValue = Integer.parseInt(retirement.getText().toString());
+        sp = getSharedPreferences(SHARED_PREFS_SALARY, MODE_PRIVATE);
+        ed = sp.edit();
+        ed.putString(shared_yearly_salary, text_yearly_salary.getText().toString());
+        ed.commit();
 
-        leave = (EditText) findViewById(R.id.text_leave_weight);
-        int leaveValue = Integer.parseInt(leave.getText().toString());
+        sp = getSharedPreferences(SHARED_PREFS_BONUS, MODE_PRIVATE);
+        ed = sp.edit();
+        ed.putString(shared_yearly_bonus, text_yearly_bonus.getText().toString());
+        ed.commit();
 
-        Weights.setCommute_weight(commute_timeText);
-        Weights.setSalary_weight(salary);
-        Weights.setBonus_weight(bonus);
-        Weights.setRetirement_weight(retirementValue);
-        Weights.setLeave_weight(leaveValue);
+        sp = getSharedPreferences(SHARED_PREFS_RETIREMENT, MODE_PRIVATE);
+        ed = sp.edit();
+        ed.putString(shared_retirement, text_retirement.getText().toString());
+        ed.commit();
 
-        Log.v("Text",Weights.printWeights());
+        sp = getSharedPreferences(SHARED_PREFS_LEAVE, MODE_PRIVATE);
+        ed = sp.edit();
+        ed.putString(shared_leave, text_leave.getText().toString());
+        ed.commit();
+    }
+
+    public void loadData() {
+        SharedPreferences sp;
+
+        sp = getSharedPreferences(SHARED_PREFS_COMMUTE, MODE_PRIVATE);
+        saved_commute_time = sp.getString(shared_commute_time, "");
+        text_commute_time.setText(saved_commute_time);
+
+        sp = getSharedPreferences(SHARED_PREFS_SALARY, MODE_PRIVATE);
+        saved_yearly_salary = sp.getString(shared_yearly_salary, "");
+        text_yearly_salary.setText(saved_yearly_salary);
+
+        sp = getSharedPreferences(SHARED_PREFS_BONUS, MODE_PRIVATE);
+        saved_yearly_bonus = sp.getString(shared_yearly_bonus, "");
+        text_yearly_bonus.setText(saved_yearly_bonus);
+
+        sp = getSharedPreferences(SHARED_PREFS_RETIREMENT, MODE_PRIVATE);
+        saved_retirement = sp.getString(shared_retirement, "");
+        text_retirement.setText(saved_retirement);
+
+        sp = getSharedPreferences(SHARED_PREFS_LEAVE, MODE_PRIVATE);
+        saved_leave = sp.getString(shared_leave, "");
+        text_leave.setText(saved_leave);
+    }
+
+    public void updateViews() {
+        text_commute_time.setText(saved_commute_time);
+        text_yearly_salary.setText(saved_yearly_salary);
+        text_yearly_bonus.setText(saved_yearly_bonus);
+        text_retirement.setText(saved_retirement);
+        text_leave.setText(saved_leave);;
     }
 }
